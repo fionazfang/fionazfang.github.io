a917155e6dce87ace984ab8e9d843a108b87a899
fionazfang <78430677+fionazfang@users.noreply.github.com>
2025-06-21 20:41:57 +0000

    Deploying to gh-pages from @ fionazfang/fionazfang.github.io@10e748d4433d37bde54da917e3e32679e5d00521 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -79,9 +79,9 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
+          section: "News",},{id: "projects-energy-balance-model",
+          title: 'Energy Balance Model',
+          description: "0D and 1D Energy Balance Model with Quaternary Radiative Forcing",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
             },},{id: "projects-project-2",
