895ae41454d38fa8b210f9d15f1f82152f9600f6
fionazfang <78430677+fionazfang@users.noreply.github.com>
2025-06-24 00:46:58 +0000

    Deploying to gh-pages from @ fionazfang/fionazfang.github.io@b3bf3c3308a5223629bb255280ee0a407d9efe87 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -94,9 +94,9 @@ ninja.data = [{
           description: "0D and 1D Energy Balance Model with Quaternary Radiative Forcing",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2024-ebm/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
+            },},{id: "projects-orbital-control-during-the-past-1-5-ma",
+          title: 'Orbital Control during the Past 1.5 Ma',
+          description: "The Influence of Seasonal and Latitudinal Insolation Distribution on Climate Variability over the Past 1.5 Ma",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
             },},{id: "projects-project-4",
